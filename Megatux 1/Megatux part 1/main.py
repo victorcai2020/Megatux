e2d977194b75f2edaa725851e46e9761811d012a
Victor Cai <victor.cai@outlook.com>
2021-10-06 07:34:07 -0400

    Third commit

--- Megatux 1/Megatux part 1/main.py
+++ Megatux 1/Megatux part 1/main.py
@@ -740,6 +740,8 @@ player, health_bar = world.process_data(world_data)
 def cutscene(position):
 	dialogue = True
 	screen.blit(text, (0, 0))
+	if position == 'start':
+		draw_text('Evil snowmen have taken over Antarctica!', font, WHITE, 40, 10)
 
 def show_credits():
 	pass
@@ -784,131 +786,131 @@ while run:
 	for x in range(player.grenades):
 		screen.blit(grenade_img, (10 + (x * 15), 60))
 
+	if dialogue == False:
+		player.update()
+		player.draw()
 
-	player.update()
-	player.draw()
+		for enemy in enemy_group:
+			enemy.ai()
+			enemy.update()
+			enemy.draw()
 
-	for enemy in enemy_group:
-		enemy.ai()
-		enemy.update()
-		enemy.draw()
+		#update and draw groups
+		bullet_group.update()
+		grenade_group.update()
+		explosion_group.update()
+		item_box_group.update()
+		decoration_group.update()
+		water_group.update()
+		exit_group.update()
+		bullet_group.draw(screen)
+		grenade_group.draw(screen)
+		explosion_group.draw(screen)
+		item_box_group.draw(screen)
+		decoration_group.draw(screen)
+		water_group.draw(screen)
+		exit_group.draw(screen)
 
-	#update and draw groups
-	bullet_group.update()
-	grenade_group.update()
-	explosion_group.update()
-	item_box_group.update()
-	decoration_group.update()
-	water_group.update()
-	exit_group.update()
-	bullet_group.draw(screen)
-	grenade_group.draw(screen)
-	explosion_group.draw(screen)
-	item_box_group.draw(screen)
-	decoration_group.draw(screen)
-	water_group.draw(screen)
-	exit_group.draw(screen)
-
-	#show intro
-	if start_intro == True:
-		if intro_fade.fade():
-			start_intro = False
-			intro_fade.fade_counter = 0
-
-
-	#update player actions
-	if player.alive:
-		#shoot bullets
-		if shoot:
-			player.shoot()
-		#throw grenades
-		elif grenade and grenade_thrown == False and player.grenades > 0:
-			grenade = Grenade(player.rect.centerx + (0.5 * player.rect.size[0] * player.direction),\
-						player.rect.top, player.direction)
-			grenade_group.add(grenade)
-			#reduce grenades
-			player.grenades -= 1
-			grenade_thrown = True
-		if player.in_air:
-			player.update_action(2)#2: jump
-		elif moving_left or moving_right:
-			player.update_action(1)#1: run
-		else:
-			player.update_action(0)#0: idle
-		screen_scroll, level_complete = player.move(moving_left, moving_right)
-		bg_scroll -= screen_scroll
-		#check if player has completed the level
-		if level_complete:
-			if level <= MAX_LEVELS:
-				start_intro = True
-				level += 1
-				bg_scroll = 0
-				world_data = reset_level()
-			if level >= MAX_LEVELS:
-				level = 0
-				run = False
-				show_credits()
-		else:
-			screen_scroll = 0
-			pygame.mixer.music.fadeout(500)
-			if death_fade.fade():
-				if restart_button.draw(screen):
-					death_fade.fade_counter = 0
-					start_intro = True
-					bg_scroll = 0
-					world_data = reset_level()
-					#load in level data and create world
-					try:
-						with open(f'level{level}_data.csv', newline='') as csvfile:
-							reader = csv.reader(csvfile, delimiter=',')
-							for x, row in enumerate(reader):
-								for y, tile in enumerate(row):
-									world_data[x][y] = int(tile)
-					except FileNotFoundError:
-						with open('level0_data.csv', newline='') as csvfile:
-							reader = csv.reader(csvfile, delimiter=',')
-							for x, row in enumerate(reader):
-								for y, tile in enumerate(row):
-									world_data[x][y] = int(tile)
-
-					world = World()
-					player, health_bar = world.process_data(world_data)
+		#show intro
+		if start_intro == True:
+			if intro_fade.fade():
+				start_intro = False
+				intro_fade.fade_counter = 0
 
 
-	for event in pygame.event.get():
-		#quit game
-		if event.type == pygame.QUIT:
-			run = False
-		#keyboard presses
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT:
-				moving_left = True
-			if event.key == pygame.K_RIGHT:
-				moving_right = True
-			if event.key == pygame.K_SPACE:
-				shoot = True
-			if event.key == pygame.K_DOWN:
-				grenade = True
-			if event.key == pygame.K_UP and player.alive:
-				player.jump = True
-				jump_fx.play()
-			if event.key == pygame.K_ESCAPE:
-				run = False
+			#update player actions
+			if player.alive:
+				#shoot bullets
+				if shoot:
+					player.shoot()
+				#throw grenades
+				elif grenade and grenade_thrown == False and player.grenades > 0:
+					grenade = Grenade(player.rect.centerx + (0.5 * player.rect.size[0] * player.direction),\
+								player.rect.top, player.direction)
+					grenade_group.add(grenade)
+					#reduce grenades
+					player.grenades -= 1
+					grenade_thrown = True
+				if player.in_air:
+					player.update_action(2)#2: jump
+				elif moving_left or moving_right:
+					player.update_action(1)#1: run
+				else:
+					player.update_action(0)#0: idle
+				screen_scroll, level_complete = player.move(moving_left, moving_right)
+				bg_scroll -= screen_scroll
+				#check if player has completed the level
+				if level_complete:
+					if level <= MAX_LEVELS:
+						start_intro = True
+						level += 1
+						bg_scroll = 0
+						world_data = reset_level()
+					if level >= MAX_LEVELS:
+						level = 0
+						run = False
+						show_credits()
+				else:
+					screen_scroll = 0
+					pygame.mixer.music.fadeout(500)
+					if death_fade.fade():
+						if restart_button.draw(screen):
+							death_fade.fade_counter = 0
+							start_intro = True
+							bg_scroll = 0
+							world_data = reset_level()
+							#load in level data and create world
+							try:
+								with open(f'level{level}_data.csv', newline='') as csvfile:
+									reader = csv.reader(csvfile, delimiter=',')
+									for x, row in enumerate(reader):
+										for y, tile in enumerate(row):
+											world_data[x][y] = int(tile)
+							except FileNotFoundError:
+								with open('level0_data.csv', newline='') as csvfile:
+									reader = csv.reader(csvfile, delimiter=',')
+									for x, row in enumerate(reader):
+										for y, tile in enumerate(row):
+											world_data[x][y] = int(tile)
+
+							world = World()
+							player, health_bar = world.process_data(world_data)
 
 
-		#keyboard button released
-		if event.type == pygame.KEYUP:
-			if event.key == pygame.K_LEFT:
-				moving_left = False
-			if event.key == pygame.K_RIGHT:
-				moving_right = False
-			if event.key == pygame.K_SPACE:
-				shoot = False
-			if event.key == pygame.K_DOWN:
-				grenade = False
-				grenade_thrown = False
+			for event in pygame.event.get():
+				#quit game
+				if event.type == pygame.QUIT:
+					run = False
+				#keyboard presses
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_LEFT:
+						moving_left = True
+					if event.key == pygame.K_RIGHT:
+						moving_right = True
+					if event.key == pygame.K_SPACE:
+						shoot = True
+					if event.key == pygame.K_DOWN:
+						grenade = True
+					if event.key == pygame.K_UP and player.alive:
+						player.jump = True
+						jump_fx.play()
+					if event.key == pygame.K_ESCAPE:
+						run = False
 
 
-	pygame.display.update()
+				#keyboard button released
+				if event.type == pygame.KEYUP:
+					if event.key == pygame.K_LEFT:
+						moving_left = False
+					if event.key == pygame.K_RIGHT:
+						moving_right = False
+					if event.key == pygame.K_SPACE:
+						shoot = False
+					if event.key == pygame.K_DOWN:
+						grenade = False
+						grenade_thrown = False
+
+
+			pygame.display.update()
 
 pygame.quit()
